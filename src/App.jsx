12bb6c5b9cff0b13ce97683f9633acf2ea7c02bb
Molly Morse <mollymorse@Mollys-Air.attlocal.net>
2024-06-21 14:48:14 -0500

    tab 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
 
 export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
   return (
     <>
-      <ContactList />
+      {selectedContactId ? <div>Selected Contact View</div> : <ContactList />}
     </>
   );
 }
